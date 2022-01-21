1fb6bad57a9ec791bbdb0d6d74b185244bfcebcc
omprakash-singh <omprakashingh11009@gmail.com>
2022-01-21 11:35:48 +0530

    button

--- js file/script.js
+++ js file/script.js
@@ -1,6 +1,10 @@
 const nav_button = document.querySelector('.fas');
 const navbar = document.querySelector('.navbar');
 
+const icon_up = function () {
+    //if()
+}
 nav_button.addEventListener('click', () => {
     navbar.classList.toggle('display-navbar');
+    nav_button.classList.toggle('fa-chevron-circle-up');
 });
